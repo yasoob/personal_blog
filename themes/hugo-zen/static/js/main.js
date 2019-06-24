209d7bb3995954e12d06f346b39b939f72f311eb
Yasoob <ykhalid@asapp.com>
2019-06-24 15:46:23 -0400

    moved some js around

--- themes/hugo-zen/static/js/main.js
+++ themes/hugo-zen/static/js/main.js
@@ -2,3 +2,8 @@ function changeValue(elementName, newValue){
   document.getElementsByName(elementName)[0].value=newValue;
   window.location.hash = "#comment-form";
 };
+
+function nav_display() {
+  var element = document.getElementById("nav");
+  element.classList.toggle("hide-nav");
+}
